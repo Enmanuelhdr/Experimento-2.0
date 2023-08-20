ff892f773092b4bbf28c4cca5360baade0e7692f
Enmanuelhdr <107286044+Enmanuelhdr@users.noreply.github.com>
2023-08-20 04:02:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('Listening on port' + 3000);
